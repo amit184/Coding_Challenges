d037a81463f7cc25c8f80fa6f83ea9dc244633da
Amitkumar Jadhav <amitjadhav184@gmail.com>
2023-04-10 01:05:39 +0530

    Finalized the challenge#2 code

--- Challenge#2/metadata.sh
+++ Challenge#2/metadata.sh
@@ -7,3 +7,13 @@ do
 res=`curl "${metadata_url}/${key}?recursive=true" -H "Metadata-Flavor: Google"`
 echo "The metadata :: {"${key}" : "${res}"} " 
 done
+
+
+#-- The gcloud cmd to execute local bash script on GCP VM --!
+
+#gcloud compute ssh user@host -- bash -s < local_script.sh
+
+            #OR
+#-- One can use startip script to execute bash script on GCE VM --!
+
+#sudo google_metadata_script_runner startup --> to rerun startup script..
