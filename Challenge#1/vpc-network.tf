3caa47530e9a596f81693db086e16a899ddc6b04
Amitkumar Jadhav <amitjadhav184@gmail.com>
2023-04-09 18:21:22 +0530

    adding changes for challenges#2

--- Challenge#1/vpc-network.tf
+++ Challenge#1/vpc-network.tf
@@ -7,14 +7,14 @@ resource "google_compute_network" "vpc-main" {
 
 resource "google_compute_subnetwork" "vpc-subnet-1" {
   name          = "subnet-1"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.2.0.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc-main.id
 }
 
 resource "google_compute_subnetwork" "vpc-subnet-2" {
   name          = "subnet-2"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "192.168.0.0/24"
   region        = "us-east4"
   network       = google_compute_network.vpc-main.id
 }
--- Challenge#2/metadata.sh
+++ Challenge#2/metadata.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-METADATA_VALUE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/image -H "Metadata-Flavor: Google")
-apt update
-apt -y install apache2
-cat <<EOF > /var/www/html/index.html
-<html><body><p>Accessing metadata value of foo: $METADATA_VALUE</p></body></html>
-EOF
+
+metadata_url="http://metadata.google.internal/computeMetadata/v1/instance"
+
+for key in $@ 
+do
+res=`curl "${metadata_url}/${key}?recursive=true" -H "Metadata-Flavor: Google"`
+echo "The metadata :: {"${key}" : "${res}"} " 
+done
