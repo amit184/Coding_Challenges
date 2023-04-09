b00236cb4e148d9a1b7b1d89eb52b6077e7fbae4
Amitkumar Jadhav <amitjadhav184@gmail.com>
2023-04-10 00:44:15 +0530

    removing un-neccessary file

--- Challenge#2/metadata1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-metadata_url="http://metadata.google.internal/computeMetadata/v1/"
-
-for key in $@ 
-do
-data_url="http://metadata.google.internal/computeMetadata/v1/${key}"
-res=`curl "http://metadata.google.internal/computeMetadata/v1/instance/${key}?recursive=true" -H "Metadata-Flavor: Google"`
-echo "The metadata :: {"${key}" : "${res}"} " 
-done
